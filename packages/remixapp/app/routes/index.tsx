a62b9304463d2fbe1366432cd6bb1cf9bb6b19a6
neemchand <neemchand@founderandlightning.com>
2023-03-09 13:27:38 +0530

    packages names changed

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,5 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import {Header} from 'fl-header';
+import {Footer} from 'fl-footer';
 
 export default function Index() {
     return (
